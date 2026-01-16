63d93538a7eecb1a521a5d5eb312ca3a77967ba2
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-16 18:46:39 +0530

    added filter option also

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
    const [coins, setCoins] = useState([]);
    const [loading, setLoading] = useState(true);
    const [error, setError] = useState(null);
-  const [limit, setLimit] = useState(10);
+   const [limit, setLimit] = useState(10);
+   const [filter, setFilter] = useState("");
 
    useEffect(() => {
       const fetchCoins = async () => {
          try {
-            const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
+            const res = await fetch(
+               `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+            );
             if (!res.ok) throw new Error("Failed to fetch data");
             const data = await res.json();
             console.log(data);
@@ -26,19 +30,26 @@ const App = () => {
       fetchCoins();
    }, [limit]);
 
+   const filteredCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(filter.toLowerCase()) ||  coin.symbol.toLowerCase().includes(filter.toLowerCase())
+   })
+
    return (
       <div>
          <h1>🚀 Crypto Dash</h1>
          {loading && <p>Loading...</p>}
          {error && <div className="error">{error}</div>}
 
-        <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+         <LimitSelector limit={limit} onLimitChange={setLimit} />
+        </div>
 
          {!loading && !error && (
             <main className="grid">
-               {coins.map((coin) => (
-                  <CoinCard   key={coin.id} coin={coin}/>
-               ))}
+               {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
+                  <CoinCard key={coin.id} coin={coin} />
+               )) : (<p>No matching coins</p>)}
             </main>
          )}
       </div>
