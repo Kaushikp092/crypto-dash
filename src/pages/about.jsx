2891875fdc409047bc6f3635be482d5400d6b9f0
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-20 22:41:50 +0530

    added link component

---
+++ src/pages/about.jsx
@@ -0,0 +1,27 @@
+const AboutPage = () => {
+   return (
+      <div className="about">
+         <h1>About Cryto Dash</h1>
+         <p>
+            Cryto Dash is a simple React application that displays live
+            crytocurrency data using the CoinGecko API.
+         </p>
+         <p>
+            You can explore the top crytocurrencies by market cap, filter by
+            name or symbol, and sort them by price, market cap, or 24-hour
+            change
+         </p>
+         <p>
+            This project is built as part of a Reacct tutorial to help you
+            understand hooks, components, state management, and integrating with
+            external APIs.
+         </p>
+         <p>
+            🚀 Future features might include detailed coin views,favorites,
+            pagination, and much more!
+         </p>
+      </div>
+   );
+};
+
+export default AboutPage;
