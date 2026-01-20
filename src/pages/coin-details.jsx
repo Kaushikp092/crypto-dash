0792596d518fb62a8a4ff52d2d0128d86e7ad497
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-21 00:23:22 +0530

    change in coin-detail UI

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router";
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -9,25 +10,55 @@ const CoinDetailsPage = () => {
    const [error, setError] = useState(null);
 
    useEffect(() => {
-    const fetchCoin = async() => {
-        try {
+      const fetchCoin = async () => {
+         try {
             const res = await fetch(`${API_URL}/${id}`);
-            if(!res.ok) throw new Error('Failed to fetch data');
+            if (!res.ok) throw new Error("Failed to fetch data");
             const data = await res.json();
-            console.log(data);
             setCoin(data);
-        } catch (err) {
+         } catch (err) {
             setError(err.message);
-        }finally{
+         } finally {
             setLoading(false);
-        }
-    }
-    fetchCoin();
+         }
+      };
+      fetchCoin();
    }, [id]);
 
    return (
       <>
-         <div>Coin Details {id}</div>
+         <div className="coin-details-container">
+            <Link to="/">⬅ Back To Home</Link>
+
+            <h1 className="coin-details-title">
+               {coin ? `${coin.name} (${coin.symbol})` : "Coin Details"}
+            </h1>
+
+            {loading && <p>Loading...</p>}
+            {error && <div className="error">❌{error}</div>}
+            {!loading && !error && (
+               <>
+                  <img
+                     src={coin.image.large}
+                     alt={coin.name}
+                     className="coin-details-image"
+                  />
+                  <p>{coin.description.en.split(". ")[0] + "."}</p>
+
+                  <div className="coin-details-info">
+                     <h3>Rank: #{coin.market_cap_rank}</h3>
+                     <h3>
+                        Current Price:{" "}
+                        {coin.market_data.current_price.usd.toLocaleString()}
+                     </h3>
+                     <h4>
+                        Market Cap: $
+                        {coin.market_data.market_cap.usd.toLocaleString()}
+                     </h4>
+                  </div>
+               </>
+            )}
+         </div>
       </>
    );
 };
